2a5b7c12412f4b94e842a2c9691c7d2ee3309a3b
arjunroy7979 <arjunroy7979@gmail.com>
2024-04-19 10:48:28 +0530

    first commit

---
+++ src/constants/categoryToken.ts
@@ -0,0 +1,8 @@
+export const categoryTokens = [
+  { categoryName: 'business', color: '#b2b2a8' },
+  { categoryName: 'entertainment', color: '#b2b2a8' },
+  { categoryName: 'health', color: '#b2b2a8' },
+  { categoryName: 'science', color: '#b2b2a8' },
+  { categoryName: 'sports', color: '#b2b2a8' },
+  { categoryName: 'technology', color: '#b2b2a8' },
+];
---
+++ src/services/api.tsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+export const BASE_URL = 'https://newsapi.org/v2/';
+
+export const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const getHeadlineByCountry = async () => {
+  axios
+    .get(
+      'https://newsapi.org/v2/top-headlines?country=in&apiKey=b1d8490bbe004ee3a49b1c5bdb6e60e4'
+    )
+    .then((response: any) => {
+      // handle success
+      return response.data;
+    })
+    .catch((error: any) => {
+      // handle error
+      console.log(error);
+    });
+};
+
+export const fetchSearchDataApi = async (query: any) => {
+  const API_KEY = 'b1d8490bbe004ee3a49b1c5bdb6e60e4';
+  try {
+    const data = await axios.get(
+      ` https://newsapi.org/v2/everything?q=${query}&sortBy=popularity&apiKey=${API_KEY}`
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
